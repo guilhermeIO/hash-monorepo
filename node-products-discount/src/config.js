3a02dc91f9eb4bc46ced7b8227501c0924f77d7c
Guilherme Maia <guilhermecmaia.io@gmail.com>
2019-12-18 15:49:32 -0300

    Update configuration for MongoDB host

--- node-products-discount/src/config.js
+++ node-products-discount/src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   LOG_TAG: '[LOG::node-products-discount]',
-  MONGO_URI: 'mongodb://root:MongoDB2019!@172.19.0.2:27017'
+  MONGO_URI: 'mongodb://root:MongoDB2019!@databases_mongodb:27017'
 }
